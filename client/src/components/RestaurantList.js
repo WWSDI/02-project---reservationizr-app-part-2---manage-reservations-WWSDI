f411fe264f65874a0e513b161fea7279446afbc3
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-10-18 11:39:35 +1300

    add: display a list of restaurants in React

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./RestaurantList.css";
 
 const RestaurantList = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:5000/restaurants");
+      const data = await response.json();
+      setRestaurants(data);
+    })();
+  }, []);
+
   return (
     <>
       <h1>Restaurants</h1>
+      {restaurants.map(({ id, image: imgURL, name, description }) => {
+        return (
+          <div key={id}>
+            <img src={imgURL} alt="restaurant"></img>
+            <h2>{name}</h2>
+            <div>{description}</div>
+          </div>
+        );
+      })}
     </>
   );
 };

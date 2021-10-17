0597998c3272ce43e15a7ca967db7e91157228b5
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-10-18 10:07:42 +1300

    add: get all restaurants route

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const RestaurantModel = require("./models/RestaurantModel");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/restaurants", async (req, res) => {
+  const allRestaurants = await RestaurantModel.find({});
+  console.log("😱",allRestaurants)
+  res.status(200).send(allRestaurants);
+});
 
 module.exports = app;

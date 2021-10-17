80a6ab80a8c2df7d58f0ef765482074900d2f835
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-10-18 10:06:16 +1300

    add: RestaurantModel

--- server/src/models/RestaurantModel.js
+++ server/src/models/RestaurantModel.js
@@ -1,1 +1,11 @@
 // FIXME: Add a Mongoose model here
+const mongoose = require("mongoose");
+
+const RestaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+});
+const RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = RestaurantModel;

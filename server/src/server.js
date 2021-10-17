f57c511856df0f2623e3deb512e5968e8f39790c
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-10-18 10:05:42 +1300

    add: server connection to mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 const port = process.env.PORT || 5000;
+const { connect } = require("mongoose");
 const app = require("./app");
+
+connect("mongodb://localhost:27017/mongo")
 
 app.listen(port, () => {
   console.log(`API server started at http://localhost:${port}`);

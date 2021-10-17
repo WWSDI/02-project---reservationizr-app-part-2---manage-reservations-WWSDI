1a33e849e4f50e26a15fe5d1d80c04baf3b8f086
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-10-18 10:33:47 +1300

    add: transform option for convert _id field to id

--- server/src/models/RestaurantModel.js
+++ server/src/models/RestaurantModel.js
@@ -1,11 +1,21 @@
 // FIXME: Add a Mongoose model here
 const mongoose = require("mongoose");
 
-const RestaurantSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-});
+const RestaurantSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
 const RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
 
 module.exports = RestaurantModel;

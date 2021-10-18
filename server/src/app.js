523f030eca30d233a4a0d25c5afc4668ebba52e4
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-10-18 17:31:49 +1300

    add: jwtCheck middleware to create reservation route

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,19 @@ const RestaurantModel = require("./models/RestaurantModel");
 const ReservationModel = require("./models/ReservationModel");
 const { celebrate, Joi, errors, Segments } = require("celebrate");
 const app = express();
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-2aqg2v5b.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "https://www.reservationizr.com",
+  issuer: "https://dev-2aqg2v5b.us.auth0.com/",
+  algorithms: ["RS256"],
+});
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +29,7 @@ app.get("/restaurants", async (req, res) => {
 // Need celebrate&joi for data validation
 app.post(
   "/reservations",
+  jwtCheck,
   celebrate({
     [Segments.BODY]: Joi.object().keys({
       partySize: Joi.number().min(1).max(30).required(),
